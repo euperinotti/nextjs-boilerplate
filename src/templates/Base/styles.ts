b96636af3d34f69d0e990c87ee2075eba3bb7bd2
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-01 17:41:09 -0300

    init

---
+++ src/templates/Base/styles.ts
@@ -0,0 +1,20 @@
+import theme from '@/styles/theme'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${theme.colors.black};
+  color: ${theme.colors.white};
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Main = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 ${theme.spacings.small};
+`
